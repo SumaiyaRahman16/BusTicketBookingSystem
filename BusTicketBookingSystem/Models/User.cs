598dc3aec4748742589713dbbb1e564ec885b0f0
rsumaiya16 <rsumaiya1602@gmail.com>
2026-05-28 01:49:25 +0600

    modfied

---
+++ BusTicketBookingSystem/Models/User.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace BusTicketBookingSystem.Models
+{
+    public class User
+    {
+ 
+        public string UserId { get; private set; } 
+        public string FullName { get; private set; }
+        public string MobileNumber { get; private set; }
+        public string EmailAddress { get; private set; }
+     
+
+
+        public User(string fullName, string mobileNumber, string emailAddress)
+        {   
+            if (string.IsNullOrWhiteSpace(fullName)) 
+                throw new ArgumentException("Name cannot be empty");
+            if (string.IsNullOrWhiteSpace(emailAddress))
+                throw new ArgumentException("You must enter an email address");
+            if (string.IsNullOrWhiteSpace(mobileNumber))
+                throw new ArgumentException("Mobile number cannot be empty");
+
+    
+            UserId = "USR-" + Guid.NewGuid().ToString().Substring(0, 8).ToUpper();
+            
+            FullName = fullName;
+            MobileNumber = mobileNumber;
+            EmailAddress = emailAddress;
+
+        }
+        
+
+        public override string ToString()
+        {
+            return $"{UserId} | {FullName} | {MobileNumber} | {EmailAddress}";
+        }
+    }
+}
